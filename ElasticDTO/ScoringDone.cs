27949a644fb4fb67b7e0d8088adc03554a0a9bd7
Realsnack <msvacina@outlook.com>
2020-03-19 21:35:51 +0100

    Added model for ScoringDone

---
+++ ElasticDTO/ScoringDone.cs
@@ -0,0 +1,40 @@
+using System;
+using Nest;
+
+namespace ElasticDTO.Models
+{
+    public class ScoringDone
+    {
+        [PropertyName("errorMessage")]
+        public ErrorMessage Error { get; set; }
+        [PropertyName("timestamp")]
+        public DateTime Timestamp { get; set; }
+        [PropertyName("scoreCode")]
+        public int ScoreCode { get; set; }
+        [PropertyName("messageType")]
+        public string JmsId { get; set; }
+        [PropertyName("riskFactors")]
+        public RiskFactor[] RiskFactors { get; set; }
+        [PropertyName("scoreNum")]
+        public string ScoreNum { get; set; }
+        [PropertyName("id")]
+        public string DionId { get; set; }
+        }
+    }
+
+    public class ErrorMessage
+    {
+        [PropertyName("message")]
+        public string Message { get; set; }
+        [PropertyName("code")]
+        public string Code { get; set; }
+    }
+
+    public class RiskFactor
+    {
+        [PropertyName("riskFactorDesc")]
+        public string RiskFactorDescription { get; set; }
+        [PropertyName("riskFactor")]
+        public string RiskFactorRule { get; set; }
+    }
+}
