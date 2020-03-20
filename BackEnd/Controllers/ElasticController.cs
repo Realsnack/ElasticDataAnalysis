cb51e3af51e9ee6b5dba1551548244601dc3013b
Realsnack <msvacina@outlook.com>
2020-03-20 22:43:48 +0100

    Adjusted logging on Elastic Controller - EDA-25

--- BackEnd/Controllers/ElasticController.cs
+++ BackEnd/Controllers/ElasticController.cs
@@ -17,17 +17,20 @@ namespace BackEnd.Controllers
         private readonly ILogger<ElasticController> _logger;
         private readonly IElastic _client;
         private readonly IConfiguration _configuration;
+        private readonly string _dateFormat;
 
         public ElasticController(ILogger<ElasticController> logger, IElastic elasticClient, IConfiguration configuration)
         {
             _logger = logger;
             _client = elasticClient;
             _configuration = configuration;
+            _dateFormat = configuration.GetValue<string>("DateTimeFormat");
         }
 
         [HttpGet]
         public EndpointVersion Get()
         {
+            _logger.LogInformation($"{LogTimeStamp()}Getting Endpoint version");
             return new EndpointVersion(_configuration);
         }
 
@@ -38,11 +41,11 @@ namespace BackEnd.Controllers
         [HttpGet("transaction")]
         public async Task<IEnumerable<TransactionEscalation>> GetTransactionEscalationsAsync()
         {
-            _logger.LogDebug("Started api/elastic/transaction.");
-            _logger.LogInformation($"{DateTime.Now}:GetTransactionEscalationsAsync()");
+            _logger.LogDebug($"{LogTimeStamp()}Started api/elastic/transaction.");
+            _logger.LogInformation($"{LogTimeStamp()}GetTransactionEscalationsAsync()");
             var escalationsResponse = await _client.GetEscalationsAsync();
             var escalations = escalationsResponse.ToList();
-            _logger.LogDebug($"Loaded {escalations.Count} from GetEscalationsAsync()");
+            _logger.LogDebug($"{LogTimeStamp()}Loaded {escalations.Count} from GetEscalationsAsync()");
 
             List<TransactionEscalation> transactionsList = new List<TransactionEscalation>();
 
@@ -54,43 +57,43 @@ namespace BackEnd.Controllers
                     var result = await _client.GetTransactionResultAsync(escalation.Sid);
                     if (transaction != null)
                     {
-                        _logger.LogDebug($"Valid transaciton");
+                        _logger.LogDebug($"{LogTimeStamp()}Valid transaction {transaction.Select(s => s.OtherParameters.DionHeaderScreeningRequestUniqueId)}");
                         var inDone = transaction.ToList();
                         var scoringDone = result.ToList();
-                        _logger.LogDebug($"Transaction is {inDone[0].JmsId} from ID{escalation.Sid}");
+                        _logger.LogDebug($"{LogTimeStamp()}Transaction is {inDone[0].JmsId} from ID{escalation.Sid}");
                         if (result != null)
                         {
-                            _logger.LogDebug($"Added TransactionEscalation JMS ID: {inDone[0].JmsId}\nDionID: {escalation.Sid}\nResult: {scoringDone[0].ScoreNum}");
+                            _logger.LogDebug($"{LogTimeStamp()}Added TransactionEscalation JMS ID: {inDone[0].JmsId}\nDionID: {escalation.Sid}\nResult: {scoringDone[0].ScoreNum}");
                             transactionsList.Add(new TransactionEscalation(inDone[0], escalation, scoringDone[0]));
                         }
                         else
-                        {
-                            _logger.LogInformation($"Couldn't get result of transaction {escalation.Sid}");
-                        }
+                            _logger.LogWarning($"{LogTimeStamp()}Couldn't get result of transaction {escalation.Sid}");
                     }
                     else
-                        _logger.LogInformation($"Couldn't get result of transaction {escalation.Sid}");
+                        _logger.LogWarning($"{LogTimeStamp()}Couldn't get result of transaction {escalation.Sid}");
                 }
                 catch (System.Exception ex)
                 {
-                    _logger.LogError(ex, "Unhandled exception");
+                    _logger.LogError(ex, $"{LogTimeStamp()}Unhandled exception");
                 }
             }
 
-            _logger.LogInformation($"{DateTime.Now}:Returining {transactionsList.Count} transactions");
+            _logger.LogInformation($"{LogTimeStamp()}Returning {transactionsList.Count} transactions");
             return transactionsList;
         }
 
         [HttpGet("errors")]
         public async Task<IEnumerable<ErrorTransaction>> GetErrorTransactionsAsync()
         {
-            _logger.LogDebug("Started api/elastic/errors.");
-            _logger.LogInformation($"{DateTime.Now}:GetErrorTransactionsAsync()");
+            _logger.LogDebug($"{LogTimeStamp()}Started api/elastic/errors.");
+            _logger.LogInformation($"{LogTimeStamp()}GetErrorTransactionsAsync()");
             var errorTransactionResponse = await _client.GetErrorTransactionsAsync();
             // var errorTransactions = errorTransactionResponse.ToList();
-            _logger.LogDebug($"Loaded {errorTransactionResponse.Count()} from GetErrorTransactionsAsync()");
+            _logger.LogDebug($"{LogTimeStamp()}Loaded {errorTransactionResponse.Count()} from GetErrorTransactionsAsync()");
 
             return errorTransactionResponse;
         }
+
+        private string LogTimeStamp() => DateTime.Now.ToString(_dateFormat) + ":\t";
     }
 }
